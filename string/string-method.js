291a6e0418b583637c00683e18a07910f9ab57a9
pruthesh2006-tech <pruthesh2006@gmail.com>
2026-06-24 14:52:50 +0530

    loops and string

---
+++ string/string-method.js
@@ -0,0 +1,33 @@
+// let str = "  Java Script    Hello  ";
+// let newstr = str.trim();
+
+// console.log(newstr);
+
+
+// string slice 
+
+// let str = "12345678";
+// console.log(str.slice(1,4));
+
+
+// string concat 
+
+// let str1 = "Hello";
+// let str2 = "World";
+
+// let result = str1 + str2; // method 2 
+// console.log(result);
+
+// // console.log(str1.concat(str2)); // method 1
+
+
+// string replace
+
+// let str = "Hello";
+// console.log(str.replace("H","y"));
+
+
+// string charAt
+
+let str = "hello";
+console.log(str.charAt(2));
---
+++ string/template-literal.js
@@ -0,0 +1,7 @@
+let product = {
+    item : "pen",
+    price : 10
+};
+
+let object = `the cost of ${product.item} is ${product.price} rupees`;
+console.log(object);
