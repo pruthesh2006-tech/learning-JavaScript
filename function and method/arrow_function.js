1e8a3177822295a0eb3f6dffd6f1a0476da7b69c
pruthesh2006-tech <pruthesh2006@gmail.com>
2026-06-26 13:39:39 +0530

    function and arrow function

---
+++ function and method/arrow_function.js
@@ -0,0 +1,22 @@
+// -> for addition
+
+// const arrowSum = (a, b) => {
+//     console.log(a+b);
+// };
+// arrowSum(5,6);
+
+
+// -> for multiplication
+
+// const arrowMul = (a, b) => {
+//     return a * b;
+// }
+// let val = arrowMul(5, 6);
+// console.log(val);
+
+
+// without parameters
+
+const printHello = () => {
+    console.log("hello");
+};
---
+++ function and method/function.js
@@ -0,0 +1,28 @@
+// -> function 
+
+// function student(){
+//     console.log("Hello World!");
+//     console.log("I am learning JS");
+// }
+
+// student();
+
+
+// -> function as parameter
+
+// function text(msg){
+//     console.log(msg);
+// }
+
+// text("Learning JS");
+
+
+// -> sum of two numbers
+
+function sum(x, y){
+    s = x + y;
+    return s;
+}
+
+let val = sum(10, 8);
+console.log(val);
