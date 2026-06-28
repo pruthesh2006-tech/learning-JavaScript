44f3a6465e1d5b426978453587b55c00a75c3962
pruthesh2006-tech <pruthesh2006@gmail.com>
2026-06-28 18:53:29 +0530

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,14 @@
+// console.dir(window);
+// console.dir(window.document);
+// console.dir(document.body);
+// console.log(document.body);
+
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+
+// let headings = document.getElementsByClassName("myClass");
+// console.dir(headings);
+// console.log(headings); 
+
+let parahs = document.getElementsByTagName("p");
+console.dir(parahs);
