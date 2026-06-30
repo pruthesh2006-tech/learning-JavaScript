c5bd35931e4ee00494a33db4bf02b23aebf32267
pruthesh2006-tech <pruthesh2006@gmail.com>
2026-06-30 18:41:30 +0530

    attributes in DOM

--- DOM/script.js
+++ DOM/script.js
@@ -29,7 +29,46 @@
 // console.dir(document.body.firstChild);
 
 // inner text
-let div = document.querySelector("div");
-console.dir(div);
+// let div = document.querySelector("div");
+// console.dir(div);
 
-let heading = document.querySelector("h1");
+// let heading = document.querySelector("h1");
+
+
+// -> attributes in dom
+
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name = div.getAttribute("name");
+// console.log(name);
+
+// let para = document.querySelector("p");
+// console.log(para.setAttribute("class", "newClass"));
+
+
+// -> style
+// let div = document.querySelector("div");
+
+
+// => insert elements
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "click me!";
+// console.log(newBtn);
+
+// let p = document.querySelector("p");
+// p.after(newBtn);
+
+
+// -> create a new heading
+let newHeading = document.createElement("h1");
+newHeading.innerHTML = "<i>Hi, I am learning JS!</i>";
+
+document.querySelector("body").prepend(newHeading);
+
+let para = document.querySelector("p");
+para.remove();
